55ce994f251587705d0c0f085fb7f1cc33345a48
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-01 15:58:39 +1100

    added stat changes

--- Assets/Scripts/Custom/Customisation.cs
+++ Assets/Scripts/Custom/Customisation.cs
@@ -20,17 +20,18 @@ public class Customisation : MonoBehaviour
     public struct Stats
 	{
 		public string statName;
-		public int statIndex;
+		public int statValue;
 		public int tempStat;
 	}
 	public Stats[] playerStats = new Stats[6];
-	public CharacterClass characterClass;
+	public CharacterClass charClass;
 	public string[] text;
 	public int index;
 	public Vector2 scr;
+	public int selectedIndex, points;
 	void Start()
     {
-		
+		points = 10;
         for	(int i = 0; i < mouthMax; i++)
 		{
 			Texture2D tempTexture = Resources.Load("Character/Mouth_" + i.ToString()) as Texture2D;
@@ -67,6 +68,7 @@ public class Customisation : MonoBehaviour
 		SetTexture("Clothes", 0);
 		SetTexture("Armour", 0);
 		SetTexture("Skin", 0);
+		
     }
 
     // Update is called once per frame
@@ -233,22 +235,129 @@ public class Customisation : MonoBehaviour
 			SetTexture("Eyes",1);
 		}
 		i++;
+		if (GUI.Button(new Rect(scr.x * 0.75f,scr.y + i * (0.5f*scr.y),scr.x,scr.y * 0.5f), "Random"))
+		{
+			SetTexture("Eyes",Random.Range(0,3));
+			SetTexture("Skin",Random.Range(0,3));
+			SetTexture("Armour",Random.Range(0,10));
+			SetTexture("Hair",Random.Range(0,4));
+			SetTexture("Clothes",Random.Range(0,10));
+			SetTexture("Mouth",Random.Range(0,2));
+		}
+		i++;
+		if (GUI.Button(new Rect(scr.x * 0.75f,scr.y + i * (0.5f*scr.y),scr.x,scr.y * 0.5f), "Default"))
+		{
+			SetTexture("Eyes",4);
+			SetTexture("Skin",4);
+			SetTexture("Armour",11);
+			SetTexture("Hair",5);
+			SetTexture("Clothes",11);
+			SetTexture("Mouth",3);
+		}
+		i++;
 		
 	}
-	void Buttons(string type, int dir)
-	{
-		
-	}
+	
 	void DisplayStats()
 	{
-		
+	characterName = GUI.TextField(new Rect(scr.x * 2f,scr.y * 6.5f,scr.x * 12,scr.y * 0.5f), characterName, 42);
+	#region StatDistribution
+	int i = 0;
+	if (GUI.Button(new Rect(scr.x * 13.75f,scr.y *(1 + i * 0.5f),scr.x * 0.5f,scr.y * 0.5f), "<"))
+		{
+			selectedIndex--;
+			if(selectedIndex < 0)
+			{
+				selectedIndex = 3;
+			}
+			ChooseClass(selectedIndex);
+		}
+		GUI.Button(new Rect(scr.x * 14.25f,scr.y *(1 + i * 0.5f),scr.x,scr.y * 0.5f), charClass.ToString());
+		if (GUI.Button(new Rect(scr.x * 15.25f,scr.y + i * (0.5f*scr.y),scr.x * 0.5f,scr.y * 0.5f), ">"))
+		{
+			selectedIndex++;
+			if(selectedIndex > 3)
+			{
+				selectedIndex = 0;
+			}
+			ChooseClass(selectedIndex);
+		}
+		i++;
+		GUI.Button(new Rect(scr.x * 14.25f,scr.y *(1 + i * 0.5f),scr.x * 2,scr.y * 0.5f), "Points: " + points);
+		for (int s = 0; s < playerStats.Length; s++)
+		{
+			if(points > 0 && playerStats[s].tempStat < 10)
+			{
+				if(GUI.Button(new Rect(scr.x * 14.25f,scr.y *(3 + s * 0.5f),scr.x/2,scr.y * 0.5f),  "+"))
+				{
+					points--;
+					playerStats[s].tempStat++;
+				}
+			}
+			GUI.Button(new Rect(scr.x * 12.25f,scr.y *(3 + s * 0.5f),scr.x * 2,scr.y * 0.5f), playerStats[s].statName + ": "+(playerStats[s].statValue+playerStats[s].tempStat));
+		if(points < 10  && playerStats[s].tempStat > -10)
+			{
+				if(GUI.Button(new Rect(scr.x * 11.75f,scr.y *(3 + s * 0.5f),scr.x/2,scr.y * 0.5f),  "-"))
+				{
+					points++;
+					playerStats[s].tempStat--;
+				}
+			}
+		}
+	#endregion
 	}
+	void ChooseClass(int className)
+	{
+		if(className < 0)
+		{
+			
+		}
+	switch(className)
+	{
+		case 0:
+		playerStats[0].statValue = 8;
+		playerStats[1].statValue = 5;
+		playerStats[2].statValue = 9;
+		playerStats[3].statValue = 3;
+		playerStats[4].statValue = 2;
+		playerStats[5].statValue = 3;
+		charClass = CharacterClass.Fighter;
+		break;
+		case 1:
+		playerStats[0].statValue = 1;
+		playerStats[1].statValue = 2;
+		playerStats[2].statValue = 6;
+		playerStats[3].statValue = 9;
+		playerStats[4].statValue = 8;
+		playerStats[5].statValue = 4;
+		charClass = CharacterClass.Mage;
+		break;
+		case 2:
+		playerStats[0].statValue = 4;
+		playerStats[1].statValue = 9;
+		playerStats[2].statValue = 5;
+		playerStats[3].statValue = 3;
+		playerStats[4].statValue = 1;
+		playerStats[5].statValue = 8;
+		charClass = CharacterClass.Thief;
+		break;
+		case 3:
+		playerStats[0].statValue = 5;
+		playerStats[1].statValue = 5;
+		playerStats[2].statValue = 5;
+		playerStats[3].statValue = 5;
+		playerStats[4].statValue = 5;
+		playerStats[5].statValue = 5;
+		charClass = CharacterClass.Normy;
+		break;
+	}
+	}
+	
 	public enum CharacterClass
 	{
 		Fighter,
 		Mage,
 		Thief,
-		Cleric,
-		Bard
+		Normy
 	}
 }
