cc3669ac0841eac11ee5b29c1c59a87123778c8c
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-06 09:12:11 +1100

    added pseudocode

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //create a gameObject called SettingsGO
     public GameObject SettingsGO;
     #region Singleton
+
     public static GameManager Instance = null;
     private void Awake()
     {
@@ -16,7 +18,7 @@ public class GameManager : MonoBehaviour
     #endregion
 
     public int score = 0; // ScoreKeeping
-
+    //create a bool called gameended
     public bool gameEnded = false;
 
     /*/public void AddScore(int scoretoAdd)
@@ -27,31 +29,35 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.UpdateScore(score);
     }
     /*/
+    //create a function that ends the game
     public void GameOver()
     {
         gameEnded = true;
     }
 
     //Reloads the Current Level
+    //create a function to restart a scene by loading the activeScene
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
+    //
+    //create a function to move to next scene by loading the activeScene +1
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    //create a function to move to previous scene by loading the activeScene - 1
     public void PrevLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
-
+    //create a function to load a scene by loading the activeScene of the levelID int
     public void SwitchLevel(int levelID)
     {
         SceneManager.LoadScene(levelID);
     }
+    //create a function to quite the game by unityEditor isPlaying false and calling a inbuilt function to make the application quit
     public void QuitGame()
     {
 #if UNITY_EDITOR
@@ -59,6 +65,7 @@ public class GameManager : MonoBehaviour
 #endif
         Application.Quit();
     }
+    //create a function to Open the settings by setting the SeetingsOpen to false
     public void OpenSettings()
     {
 
@@ -73,8 +80,10 @@ public class GameManager : MonoBehaviour
 
 
     }
+    //create an update function that make SettingsGO option switch between active and inactive based on whether SettingsOpen is true or false
     void Update()
     {
+        
         if (Setting.SettingsOpen)
         {
             SettingsGO.SetActive(true);
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    //create a static bool called isPaused
     public static bool isPaused = false;
+    //create a gameObject called _pauseMenu
     public GameObject _pauseMenu;
     // Start is called before the first frame update
   
@@ -12,13 +14,18 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //if you press the escape button
+        //activate the TogglePause function
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
         }
     }
+    //create a function called TogglePause
     public void TogglePause()
     {
+        //if its paused
+        //make _pauseMenu inactive, set isPaused to false, make timeScale normal, lock the mouse cursor and make it invisible
         if (isPaused)
         {
             Time.timeScale = 1;
@@ -29,6 +36,8 @@ public class PauseMenu : MonoBehaviour
             return;
 
         }
+        //if not paused
+        //make _pauseMenu active, set isPaused to true, make timeScale zero, unlock the mouse cursor and make it visible
         else
         {
             Time.timeScale = 0;
